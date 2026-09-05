851e53fd1a79425dc6a6a73eb4e970b95762a1ae
ras@fedora <ras@fedora>
2026-09-05 12:08:53 +0100

    feat: refactor

--- templates/python-lib/template/src/{{ package_name }}/__init__.py
+++ templates/python-lib/template/src/{{ package_name }}/__init__.py
@@ -1,1 +1,13 @@
-__version__ = "0.1.0"
+# from package_name.x import y
+# from package_name.b import (
+#     d,
+#     e,
+#     f,
+# )
+#
+# __all__ = [
+#     "y",
+#     "d",
+#     "e",
+#     "f",
+# ]
